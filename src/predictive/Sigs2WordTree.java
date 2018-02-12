435c6fa9b327ecf92205d81ff57fe80283c8abd7
Unknown <connorlwilkes@gmail.com>
2018-02-12 16:56:28 +0000

    added Sigs2WordTree class

---
+++ src/predictive/Sigs2WordTree.java
@@ -0,0 +1,24 @@
+/**
+ * A command line program that converts a list of input signatures from the command line to a tree of matching signatures
+ *
+ * @author Connor Wilkes
+ * @version 7/2/2018
+ */
+
+package predictive;
+
+public class Sigs2WordTree {
+
+    public static void main(String[] args) {
+
+        TreeDictionary dictionary = new TreeDictionary("dictionary");
+
+        for (String signature : args) {
+
+            System.out.println(signature + " " + dictionary.signatureToWords(signature));
+        }
+    }
+
+
+
+}
