319c0493fdbc5937bde09231fa204e2cfc8179c3
Unknown <connorlwilkes@gmail.com>
2018-02-14 16:57:54 +0000

    updated DictionaryModel

--- src/predictive/DictionaryModel.java
+++ src/predictive/DictionaryModel.java
@@ -1,47 +1,72 @@
 package predictive;
 
-
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Observable;
 
-public class DictionaryModel implements DictionaryModelInterface {
+import static predictive.HelperMethods.wordToSignature;
 
-    TreeDictionary dictionary;
+public class DictionaryModel extends Observable implements DictionaryModelInterface {
+
+    private TreeDictionary dictionary;
+    private StringBuilder word;
+    private List<String> message;
+    private List<String> matches;
+    private String currentMatch;
+    private int indexCounter;
 
 
     public DictionaryModel(String dictionaryFile) {
-
-        this.dictionary = new TreeDictionary(dictionaryFile);
-
+        dictionary = new TreeDictionary(dictionaryFile);
+        setUp();
     }
 
     public DictionaryModel() {
+        dictionary = new TreeDictionary("dictionary");
+        setUp();
+    }
 
-        this.dictionary = new TreeDictionary("dictionary");
-
+    public void setUp() {
+        word = new StringBuilder();
+        message = new ArrayList<>();
+        matches = new ArrayList<>();
+        indexCounter = 0;
     }
 
     @Override
     public List<String> getMessage() {
-        return null;
+        return message;
     }
 
     @Override
     public void addCharacter(char key) {
-
+        word.append(key);
+        matches.addAll(dictionary.signatureToWords(wordToSignature(word.toString())));
+        matches.sort(String::compareTo);
+        indexCounter = 0;
     }
 
     @Override
     public void removeLastCharacter() {
-
+        word.delete(word.length() - 1, word.length() - 1);
+        matches.addAll(dictionary.signatureToWords(wordToSignature(word.toString())));
+        matches.sort(String::compareTo);
+        indexCounter = 0;
     }
 
     @Override
     public void nextMatch() {
+        currentMatch = matches.get(indexCounter);
 
+        if (indexCounter == (matches.size() - 1)) {
+            indexCounter = 0;
+        } else {
+            indexCounter =+ 1;
+        }
     }
 
     @Override
     public void acceptWord() {
-
+        message.add(word.toString());
     }
 }
