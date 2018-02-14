71a62db4098c8a6d2429382a7601fa0b75b10f67
Unknown <connorlwilkes@gmail.com>
2018-02-14 16:04:53 +0000

    added gui classes

---
+++ src/predictive/DictionaryModel.java
@@ -0,0 +1,16 @@
+package predictive;
+
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
+
+public class Model extends Observable {
+    @Override
+    public void addListener(InvalidationListener listener) {
+
+    }
+
+    @Override
+    public void removeListener(InvalidationListener listener) {
+
+    }
+}
---
+++ src/predictive/View.java
@@ -0,0 +1,4 @@
+package predictive;
+
+public class View {
+}
